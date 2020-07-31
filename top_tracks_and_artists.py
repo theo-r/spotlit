87ece7ef1937cc4644b7401106ac35b8be92e45f
theo-r <theo.rutter@gmail.com>
2020-07-31 10:51:10 +0100

    Add catch blocks to reduce error output

--- top_tracks_and_artists.py
+++ top_tracks_and_artists.py
@@ -40,6 +40,7 @@ def create_aggs_dict(name):
         return 'first'
 
 urlPara = None
+token = None
 
 sessions = Server.get_current()._session_info_by_id
 session_id_key = list(sessions.keys())[0]
@@ -52,41 +53,49 @@ auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path(), scop
 auth_url = auth_manager.get_authorize_url()
 st.markdown(f'[Log in to Spotify]({auth_url})')
 
-urlPara = session.ws.request.connection.params.urlpara
+try:
+    urlPara = session.ws.request.connection.params.urlpara
+except AttributeError:
+    urlPara = None
 
-auth_manager.get_access_token(urlPara['code'])
+try:
+    token = auth_manager.get_access_token(urlPara['code'])
+except TypeError:
+    token = None
 
-# Step 4. Signed in, display data
-sp = spotipy.Spotify(auth_manager=auth_manager)
+if token is not None:
+    
+    # Step 4. Signed in, display data
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
-@st.cache(allow_output_mutation=True)
-def prepare_data():
-    tracks_short = prep_tracks_data(sp.current_user_top_tracks(limit=50, time_range='short_term'))
-    tracks_medium = prep_tracks_data(sp.current_user_top_tracks(limit=50, time_range='medium_term'))
-    tracks_long = prep_tracks_data(sp.current_user_top_tracks(limit=50, time_range='long_term'))
+    @st.cache(allow_output_mutation=True)
+    def prepare_data():
+        tracks_short = prep_tracks_data(sp.current_user_top_tracks(limit=50, time_range='short_term'))
+        tracks_medium = prep_tracks_data(sp.current_user_top_tracks(limit=50, time_range='medium_term'))
+        tracks_long = prep_tracks_data(sp.current_user_top_tracks(limit=50, time_range='long_term'))
 
-    artists_short = prep_artists_data(sp.current_user_top_artists(limit=50, time_range='short_term'))
-    artists_medium = prep_artists_data(sp.current_user_top_artists(limit=50, time_range='medium_term'))
-    artists_long = prep_artists_data(sp.current_user_top_artists(limit=50, time_range='long_term'))
+        artists_short = prep_artists_data(sp.current_user_top_artists(limit=50, time_range='short_term'))
+        artists_medium = prep_artists_data(sp.current_user_top_artists(limit=50, time_range='medium_term'))
+        artists_long = prep_artists_data(sp.current_user_top_artists(limit=50, time_range='long_term'))
 
-    artists_short.rename(columns={'name': 'short_term'}, inplace=True)
-    artists_medium.rename(columns={'name': 'medium_term'}, inplace=True)
-    artists_long.rename(columns={'name': 'long_term'}, inplace=True)
+        artists_short.rename(columns={'name': 'short_term'}, inplace=True)
+        artists_medium.rename(columns={'name': 'medium_term'}, inplace=True)
+        artists_long.rename(columns={'name': 'long_term'}, inplace=True)
 
-    artists = pd.concat([artists_short, artists_medium, artists_long], axis=1)
+        artists = pd.concat([artists_short, artists_medium, artists_long], axis=1)
 
-    return tracks_short, tracks_medium, tracks_long, artists
+        return tracks_short, tracks_medium, tracks_long, artists
 
-tracks_short, tracks_medium, tracks_long, artists = prepare_data()
+    tracks_short, tracks_medium, tracks_long, artists = prepare_data()
 
-st.write('Top tracks (short-term)')
-st.write(tracks_short[['name', 'artists.name', 'album.name']][:25])
+    st.write('Top tracks (short-term)')
+    st.write(tracks_short[['name', 'artists.name', 'album.name']][:25])
 
-st.write('Top tracks (medium-term)')
-st.write(tracks_medium[['name', 'artists.name', 'album.name']][:25])
+    st.write('Top tracks (medium-term)')
+    st.write(tracks_medium[['name', 'artists.name', 'album.name']][:25])
 
-st.write('Top tracks (long-term)')
-st.write(tracks_long[['name', 'artists.name', 'album.name']][:25])
+    st.write('Top tracks (long-term)')
+    st.write(tracks_long[['name', 'artists.name', 'album.name']][:25])
 
-st.write('Top artists')
-st.write(artists[['short_term', 'medium_term', 'long_term']][:25])
+    st.write('Top artists')
+    st.write(artists[['short_term', 'medium_term', 'long_term']][:25])
